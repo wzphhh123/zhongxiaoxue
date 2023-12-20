bc277ac25ee185ad41f46b9f9770df2eb21cefbb
kebai547 <17531372076@163.com>
2023-12-20 17:29:01 +0800

    增加地图、饼图demo

--- src/router/index.js
+++ src/router/index.js
@@ -3,31 +3,41 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter)
 
 const routes = [
-    {
-        path: "/",
-        name: "Login",
-        component: () => import("@/view/login/index.vue"),
-        meta: {
-            keepAlive: true,
-            activeMenu: 'Login',
-            hidden: false,
-            name: '注册登录'
-        }
-    },
-    {
-        path: "/bigData",
-        name: "bigData",
-        component: () => import("@/view/bigData/index.vue"),
-        meta: {
-            keepAlive: true,
-            activeMenu: 'bigData',
-            hidden: false,
-            name: '大数据'
-        }
-    },
+	{
+		path: "/",
+		name: "Login",
+		component: () => import("@/view/login/index.vue"),
+		meta: {
+			keepAlive: true,
+			activeMenu: 'Login',
+			hidden: false,
+			name: '注册登录'
+		}
+	},
+	{
+		path: "/bigData",
+		name: "bigData",
+		component: () => import("@/view/bigData/index.vue"),
+		meta: {
+			keepAlive: true,
+			activeMenu: 'bigData',
+			hidden: false,
+			name: '大数据'
+		}
+	},
+	{
+		path: "/test1",
+		name: "test1",
+		component: () => import("@/view/test/test1.vue"),
+	},
+	{
+		path: "/test2",
+		name: "test2",
+		component: () => import("@/view/test/test2.vue"),
+	},
 ]
 const router = new VueRouter({
-    routes,
+	routes,
 })
 
 export default router
