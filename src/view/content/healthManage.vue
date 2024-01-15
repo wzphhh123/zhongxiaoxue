b26bfec1212f7056b3a7dabaff115f3efc62a519
wzp <1085588370@qq.com>
2024-01-15 20:11:09 +0800

    用户健康新增字段展示

--- src/view/content/healthManage.vue
+++ src/view/content/healthManage.vue
@@ -9,18 +9,18 @@
           :wrapper-col="wrapperCol"
           layout="inline"
         >
-          <a-form-model-item label="用户信息">
+          <a-form-model-item label="身份证号">
             <a-input
               v-model="searchForm.identity"
               style="width: 250px"
-              placeholder="请输入用户信息"
+              placeholder="请输入身份证号"
               allow-clear
             />
           </a-form-model-item>
         </a-form-model>
         <a-button type="primary" @click="userhealth()">查询</a-button>
         <!-- <a-button type="primary" @click="reset()">重置</a-button> -->
-        <a-button type="primary" @click="addVisible = true">新增</a-button>
+        <!-- <a-button type="primary" @click="addVisible = true">新增</a-button> -->
       </div>
       <div class="main">
         <a-table
@@ -29,7 +29,12 @@
           :pagination="pagination"
           @change="tablePageChange"
           bordered
+          :scroll="{ x: 2340 }"
         >
+          <template slot="purchase" slot-scope="text">
+            <span v-if="text == 0" >未购买</span>
+            <span v-if="text == 1" >购买</span>
+          </template>
           <template slot="state" slot-scope="text">
             <span v-if="text == 0" style="color: red">异常</span>
             <span v-if="text == 1" style="color: #10c30c">健康</span>
@@ -74,20 +79,36 @@
         :label-col="labelCol2"
         :wrapper-col="wrapperCol2"
       >
-        <a-form-model-item label="用户信息" prop="identity">
+        <a-form-model-item label="身份证号" prop="identity">
           <a-input v-model="addForm.identity" />
         </a-form-model-item>
         <a-form-model-item label="心率(次/分钟)" prop="heartRate">
-          <a-input-number  v-model="addForm.heartRate" :min="1" style="width:236px" />
+          <a-input-number
+            v-model="addForm.heartRate"
+            :min="1"
+            style="width: 236px"
+          />
         </a-form-model-item>
         <a-form-model-item label="血氧(mol/l)" prop="bloodOxygen">
-          <a-input-number  v-model="addForm.bloodOxygen" :min="1"  style="width:236px"/>
+          <a-input-number
+            v-model="addForm.bloodOxygen"
+            :min="1"
+            style="width: 236px"
+          />
         </a-form-model-item>
         <a-form-model-item label="身高(cm)" prop="stature">
-          <a-input-number  v-model="addForm.stature" :min="1" style="width:236px" />
+          <a-input-number
+            v-model="addForm.stature"
+            :min="1"
+            style="width: 236px"
+          />
         </a-form-model-item>
         <a-form-model-item label="体重(Kg)" prop="weight">
-          <a-input-number  v-model="addForm.weight" :min="1" style="width:236px" />
+          <a-input-number
+            v-model="addForm.weight"
+            :min="1"
+            style="width: 236px"
+          />
         </a-form-model-item>
         <a-form-model-item label="选择状态" prop="state">
           <a-select v-model="addForm.state" placeholder="请选择状态">
@@ -110,28 +131,96 @@ export default {
       wrapperCol2: { span: 12 },
       columns: [
         {
-          title: "用户信息",
+          title: "身份证号",
           dataIndex: "identity",
           align: "center",
+          fixed: "left",
+          width:100,
+        },
+        {
+          title: "姓名",
+          dataIndex: "name",
+          align: "center",
+          width: 100,
+        },
+        {
+          title: "用户编号",
+          dataIndex: "softwareNumber",
+          align: "center",
+          width: 100,
+        },
+        { title: "学号", dataIndex: "student", align: "center", width: 100 },
+        {
+          title: "手机号",
+          dataIndex: "phone",
+          align: "center",
+          width: 100,
         },
         {
           title: "心率（次/分钟）",
           dataIndex: "heartRate",
           align: "center",
+          width: 100,
         },
-        { title: "血氧（mol/l）", dataIndex: "bloodOxygen", align: "center" },
-        { title: "身高(cm)", dataIndex: "stature", align: "center" },
-        { title: "体重(Kg)", dataIndex: "weight", align: "center" },
+        {
+          title: "血氧（mol/l）",
+          dataIndex: "bloodOxygen",
+          align: "center",
+          width: 100,
+        },
+        {
+          title: "身高(cm)",
+          dataIndex: "stature",
+          align: "center",
+          width: 100,
+        },
+        { title: "体重(斤)", dataIndex: "weight", align: "center", width: 100 },
+        { title: "体温", dataIndex: "animal", align: "center", width: 100 },
+        { title: "基础代谢", dataIndex: "basal", align: "center", width: 100 },
+        { title: "骨量", dataIndex: "bone", align: "center", width: 100 },
+        { title: "脂肪率", dataIndex: "fat", align: "center", width: 100 },
+        {
+          title: "健康指数",
+          dataIndex: "healthIndex",
+          align: "center",
+          width: 100,
+        },
+        { title: "水分率", dataIndex: "moisture", align: "center", width: 100 },
+        { title: "肌肉率", dataIndex: "muscle", align: "center", width: 100 },
+        { title: "人体质量", dataIndex: "people", align: "center", width: 100 },
+        { title: "体年龄", dataIndex: "physical", align: "center", width: 100 },
+        { title: "蛋白质", dataIndex: "protein", align: "center", width: 100 },
+        {
+          title: "皮下脂肪",
+          dataIndex: "subcutaneous",
+          align: "center",
+          width: 100,
+        },
+        {
+          title: "内脏脂肪",
+          dataIndex: "visceral",
+          align: "center",
+          width: 100,
+        },
+        {
+          title: "购买状态",
+          dataIndex: "purchase",
+          align: "center",
+           width: 100,
+          scopedSlots: { customRender: "purchase" },
+        },
         {
           title: "状态",
           dataIndex: "state",
           align: "center",
+          fixed: "right",
           scopedSlots: { customRender: "state" },
         },
         {
           title: "操作",
           dataIndex: "operation",
           align: "center",
+          fixed: "right",
           scopedSlots: { customRender: "operation" },
         },
       ],
@@ -139,7 +228,7 @@ export default {
         identity: [
           {
             required: true,
-            message: "请填写用户信息",
+            message: "请填写身份证号",
             trigger: "blur",
           },
         ],
